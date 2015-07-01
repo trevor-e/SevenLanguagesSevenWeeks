2e86313616bc9c8c31792ec84600241473ff3bae
Trevor Elkins <elkins.trevor@gmail.com>
2015-06-30 22:00:41 -0400

    clean things up a bit, and add cabal file.

--- Haskell/src/Boggle.hs
+++ Haskell/src/Boggle.hs
@@ -5,8 +5,9 @@ import Data.List.Split
 
 type Coord = (Int, Int)
 type Path = [Coord]
-type Board = [Char]
-type Word = [Char]
+type Board = String
+type Word = [Letter]
+type Letter = Char
 
 boardSide = 4
 
@@ -22,11 +23,14 @@ posToCoord pos = (pos `div` 4, pos `mod` 4)
 coordToIndex :: Coord -> Int
 coordToIndex (x,y) = x + (y * boardSide)
 
+letterAt :: Board -> Path -> Letter
+letterAt board path = board !! (coordToIndex (head path))
+
 findSubString :: Board -> Word -> Path -> Path
 findSubString board word path
-    | (head word)   /= (board !! (coordToIndex (head path))) = []             -- stop traversal if letter doesn't match
-    | (length word) == 1                                     = (-1,-1):path   -- base case for a match.  prepend (-1,-1) for splitting.
-    | otherwise                                              =                -- otherwise, continue with recursive traversal
+    | (head word)   /= letterAt board path = []             -- stop traversal if letter doesn't match
+    | (length word) == 1                   = (-1,-1):path   -- base case for a match.  prepend (-1,-1) for splitting.
+    | otherwise                            =                -- otherwise, continue with recursive traversal
         concat (map (\x -> findSubString board (tail word) (x:path)) validNeighbors)
     where validNeighbors = filter (\x -> not (x `elem` path)) (getNeighbors (head path))
 
